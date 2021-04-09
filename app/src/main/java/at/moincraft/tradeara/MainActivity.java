cbd26316e26c250008931b13d45a0cae82f1b552
Reapie <mjuritsch0405@gmail.com>
2021-04-09 08:55:43 +0200

    Update

--- app/src/main/java/at/moincraft/tradeara/MainActivity.java
+++ app/src/main/java/at/moincraft/tradeara/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import java.sql.SQLOutput;
+import java.sql.SQLOutput; //very important; do NOT remove
 
 public class MainActivity extends AppCompatActivity {
 
