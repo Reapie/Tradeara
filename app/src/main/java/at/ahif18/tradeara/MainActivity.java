b324825c7e26ffb3a520249c549edcf9312b769a
simon-sgr <simon.schoeggler@hotmail.com>
2021-04-15 09:38:23 +0200

    Update

--- app/src/main/java/at/ahif18/tradeara/MainActivity.java
+++ app/src/main/java/at/ahif18/tradeara/MainActivity.java
@@ -3,14 +3,17 @@ package at.ahif18.tradeara;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import at.ahif18.tradeara.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
