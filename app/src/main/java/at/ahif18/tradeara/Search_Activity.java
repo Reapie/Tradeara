fe26bb9cff5151ead073d3545832ed984fcd65aa
simon-sgr <simon.schoeggler@hotmail.com>
2021-04-15 08:25:14 +0200

    Update

---
+++ app/src/main/java/at/ahif18/tradeara/Search_Activity.java
@@ -0,0 +1,14 @@
+package at.ahif18.tradeara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Search_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
