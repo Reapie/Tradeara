d74d5022dbcdeb22f9795d5da82fc80a684fe11b
simon-sgr <simon.schoeggler@hotmail.com>
2021-04-15 10:14:49 +0200

    Update

--- app/src/main/java/at/ahif18/tradeara/MainActivity.java
+++ app/src/main/java/at/ahif18/tradeara/MainActivity.java
@@ -1,19 +1,28 @@
 package at.ahif18.tradeara;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import at.ahif18.tradeara.R;
+import at.ahif18.tradeara.bl.StockAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView rvStock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        rvStock = findViewById(R.id.rvStock);
+        rvStock.setHasFixedSize(true);
+
+        rvStock.setLayoutManager(new LinearLayoutManager(this));
+        rvStock.setAdapter(new StockAdapter());
     }
 }
