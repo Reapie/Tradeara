14fe76bb0413f41425908026d24556427dc366e0
moinity <david-brannan@outlook.at>
2021-04-20 09:32:45 +0200

    Fragment Update

--- app/src/main/java/at/ahif18/tradeara/fragments/BuySellFragment.java
+++ app/src/main/java/at/ahif18/tradeara/fragments/BuySellFragment.java
@@ -61,7 +61,7 @@ public class BuySellFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_buy_sell, container, false);
+        return inflater.inflate(R.layout.layout_bottom_sheet, container, false);
     }
 
     public boolean allowBackPress(){
