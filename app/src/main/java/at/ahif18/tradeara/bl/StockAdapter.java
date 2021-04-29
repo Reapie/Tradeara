b7248fb1a3b2e0ed3d77b1a0f2e7d9613f0f1636
names <reimaa17@htl-kaindorf.at>
2021-04-29 09:03:42 +0200

    idk

--- app/src/main/java/at/ahif18/tradeara/bl/StockAdapter.java
+++ app/src/main/java/at/ahif18/tradeara/bl/StockAdapter.java
@@ -29,12 +29,11 @@ public class StockAdapter extends RecyclerView.Adapter<StockHolder> {
         this.mainActivity = mainActivity;
 
         stocks = Arrays.asList(
-                new Stock("Simon", "SMN",25.26, 24.24),
-                new Stock("David", "DVD", 24.24, -25.24),
-                new Stock("Manu", "MXN", 26.24, -20.24),
-                new Stock("Martin", "MAN", 30.24, -10.24),
-                new Stock("Bene", "BNN", 150.88, 151.2),
-                new Stock("Schmidl", "SMD", 3.88, -5.23)
+                new Stock("Tesla", "TSL",25.26, 24.24),
+                new Stock("Mercedes-Benz", "BNZ", 24.24, -25.24),
+                new Stock("Ford", "Frd", 26.24, -20.24),
+                new Stock("Apple Inc.", "APL", 30.24, -10.24),
+                new Stock("Oneplus", "ONP", 150.88, 151.2)
         );
 
         stocksAll = new ArrayList<>(stocks);
